20a37bfb25d10a7102c9e611e852f0ce40cac6bb
dumetskiy <dumetskiy@gmail.com>
2020-12-16 02:03:14 +0300

    Debug update

--- src/DTO/Command/StringCommandArgumentDTO.php
+++ src/DTO/Command/StringCommandArgumentDTO.php
@@ -11,6 +11,6 @@ class StringCommandArgumentDTO extends AbstractCommandArgumentDTO
      */
     public function getFormattedForShellValue(): string
     {
-        return $this->getValue();
+        return sprintf('"%s"', $this->getValue());
     }
 }
